44edc445382bc1980ad7d8547756dd53bf102d23
Roy Mbaya <roymbaya08@gmail.com>
2023-04-18 12:41:45 +0300

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
     </nav>
   );
 }
+
+export default NavBar;
